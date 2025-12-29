7ce26067c01de3361d5370a632d3697d3eb0ef9b
dhararushi6 <arushidhar65@gmail.com>
2025-12-29 16:55:28 +0530

    Update routes.py

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 import requests
-from .parser import extract_commands
+from .parser import extract_packages
 from backend.core.scanner import PackageScanner
 
 router = APIRouter()
